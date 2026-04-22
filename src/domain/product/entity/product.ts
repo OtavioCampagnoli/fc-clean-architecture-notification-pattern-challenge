f16df34c64d0aea006485784ecdaff08e63579e2
Otavio Campagnoli <otaviocampagnoli@hotmail.com>
2026-04-22 17:13:39 -0300

    chore: add base project

---
+++ src/domain/product/entity/product.ts
@@ -0,0 +1,45 @@
+import Entity from "../../@shared/entity/entity.abstract";
+import ProductValidatorFactory from "../factory/product.validator.factory";
+
+export default class Product extends Entity  {
+  private _id: string;
+  private _name: string;
+  private _price: number;
+
+  constructor(id: string, name: string, price: number) {
+    super();
+    this._id = id;
+    this._name = name;
+    this._price = price;
+    this.validate();
+  }
+
+  get id(): string {
+    return this._id;
+  }
+  
+  get name(): string {
+    return this._name;
+  }
+
+  get price(): number {
+    return this._price;
+  }
+
+  changeName(name: string): void {
+    this._name = name;
+    this.validate();
+  }
+
+  changePrice(price: number): void {
+    this._price = price;
+    this.validate();
+  }
+
+  validate() {
+    ProductValidatorFactory.create().validate(this);
+    if (this.notification.hasErrors()) {
+      throw new Error(this.notification.messages());
+    }
+  }
+}
